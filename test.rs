dbd228260c6fb5014e33df396a472fd02ba1a011
Tertle950 <tertle950@gmail.com>
2022-09-21 20:25:27 -0400

    ayyy more rust testing

--- test.rs
+++ test.rs
@@ -1,7 +1,9 @@
 fn main() { // I WAS PROMISED NO BOILERPLATE!!! WHAT DO YOU MEAN fn main() { ????
     let x = "Hello";
     {
-        let x = "Goodbye";
+        let x = "Goodbye"; // This version of x is local to this block.
+        //                  The epitome of a temporary variable.
+        println!("{x} Heaven...")
     }
-    println!("{} World!",x);
+    println!("{} World!",x); // Prints "Hello" for x, now that we're outside that block.
 }
