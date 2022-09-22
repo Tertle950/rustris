ea9905ae948fa2f0c65856531f20798ef1406b16
Tertle950 <tertle950@gmail.com>
2022-09-21 20:38:28 -0400

    extra semicolon

--- test.rs
+++ test.rs
@@ -3,7 +3,7 @@ fn main() { // I WAS PROMISED NO BOILERPLATE!!! WHAT DO YOU MEAN fn main() { ???
     {
         let x = "Goodbye"; // This version of x is local to this block.
         //                  The epitome of a temporary variable.
-        println!("{x} Heaven...")
+        println!("{x} Heaven...");
     }
     println!("{} World!",x); // Prints "Hello" for x, now that we're outside that block.
 }
